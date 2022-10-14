f557d079ee30e48ff118a9a6ad68071ca2a17810
GeoSauer <geoffrey.sauer89@gmail.com>
2022-10-14 11:43:09 -0600

    add supabase url/key, update supabase link to 1.35.7

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,5 +1,6 @@
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://crnnmnapmmetoqehutpd.supabase.co/';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNybm5tbmFwbW1ldG9xZWh1dHBkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjU3Njg4NDcsImV4cCI6MTk4MTM0NDg0N30.fo1G97TgzcXaUZCbWMKmGQUMITR-uzpb73uwp7N4UaU';
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 /* Auth related functions */
